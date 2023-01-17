75fa01eb667b2709abcdc7983772d99145f89714
Virginie Dourson <virginiedourson@gmail.com>
2023-01-17 10:57:32 +0100

    début display

---
+++ display.php
@@ -0,0 +1,25 @@
+
+<div class="article">
+    <img src="" class="article__img">
+    <div class="article__underimg">
+        <h3 class="article__name"></h3>
+        <p class="article__price"></p>
+                                                                      
+    </div>
+</div>
+
+<?php
+// $items = [
+//     [
+//         'id' => 1,
+//         'product' => 'Nike Air Max 270',
+//         'price' => 140,
+//         'image_url' => '/assets/img/nike-air-max-279.jpeg', 
+//     ],
+//     // ...
+// ];
+
+foreach ($items as $item) {
+
+
+}
