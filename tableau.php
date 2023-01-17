dd3160a726d5ef2d056aade50cbeb448e1e69a5b
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2023-01-17 10:39:38 +0100

    creation articles

---
+++ tableau.php
@@ -0,0 +1,39 @@
+<?php
+//liste des articles
+$items = [
+    [
+        'id'=> 0 ,
+        'product' => 'Nike Air Max 270',
+        'price' => 140,
+        'image_url' => '/assets/image/shoe_one.png'
+    ],
+    [
+        'id' => 1,
+        'product' => 'Nike Air Max 280',
+        'price' => 120,
+        'image_url' => '/assets/image/shoe_two.png',
+    ],
+    [
+        'id' => 2,
+        'product' => 'Nike Air Max 290',
+        'price' => 130,
+        'image_url' => '/assets/image/shoe_three.png'
+    ],
+    [
+        'id' => 3,
+        'product' => 'Nike Air Max 120',
+        'price' => 60,
+        'image_url' => '/assets/image/shoe_four.png'
+    ],
+    [
+        'id' => 4,
+        'product' => 'Nike Air Max 200',
+        'price' => 110,
+        'image_url' => '/assets/image/shoe_five.png'
+    ],
+
+];
+
+echo "<pre>";
+print_r($items);
+echo "</pre>";
