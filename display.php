910ebbdd7d408d576af4ee3339b6cb7e158d2f2f
Virginie Dourson <virginiedourson@gmail.com>
2023-01-17 12:31:48 +0100

    submit add to cart

--- display.php
+++ display.php
@@ -1,12 +1,15 @@
-
 <!-- <div class="article">
     <img src="" class="article__img">
     <h3 class="article__name"></h3>
     <p class="article__price"></p>
-    <button class="article__addcart">Add to cart</button>                                                                    
+    <form method="get" action="display.php" class="article_addcart"> 
+        <input type="submit" name="" value="Add to cart">
+    </form>                                                                    
 </div> -->
 
 <?php
+session_start();
+$shopping_cart = $_SESSION['shopping-cart'];
 
 include "tableau.php";
 
@@ -15,7 +18,16 @@ foreach ($items as $item) {
     <img src="'.$item['image_url'].'" class="article__img">
     <h3 class="article__name">'.$item['product'].'</h3>
     <p class="article__price">'.$item['price'].'€</p>
-    <button class="article__addcart">Add to cart</button>                                                                    
+    <form method="get" action="display.php" class="article_addcart"> 
+        <input type="submit" name="Add'.$item['id'].'" value="Add to cart">
+    </form>                                                                    
     </div>';
+    if (isset($_GET["Add{$item['id']}"])) {
+        // echo 'ca marche'; 
+        // $array_push($shopping_cart, 'test');
+        // echo '<pre>';
+        // print_r($shopping_cart);
+        // echo '</pre>';
+    }
 
 }
--- tableau.php
+++ tableau.php
@@ -5,31 +5,31 @@ $items = [
         'id'=> 1 ,
         'product' => 'Nike Air Max 270',
         'price' => 140,
-        'image_url' => './assets/image/shoe_one.png'
+        'image_url' => '/assets/image/shoe_one.png'
     ],
     [
         'id' => 2,
         'product' => 'Nike Air Max 280',
         'price' => 120,
-        'image_url' => './assets/image/shoe_two.png',
+        'image_url' => '/assets/image/shoe_two.png',
     ],
     [
         'id' => 3,
         'product' => 'Nike Air Max 290',
         'price' => 130,
-        'image_url' => './assets/image/shoe_three.png'
+        'image_url' => '/assets/image/shoe_three.png'
     ],
     [
         'id' => 4,
         'product' => 'Nike Air Max 120',
         'price' => 60,
-        'image_url' => './assets/image/shoe_four.png'
+        'image_url' => '/assets/image/shoe_four.png'
     ],
     [
         'id' => 5,
         'product' => 'Nike Air Max 200',
         'price' => 110,
-        'image_url' => './assets/image/shoe_five.png'
+        'image_url' => '/assets/image/shoe_five.png'
     ],
 
 ];
