d66d62a1d9eaa336ea580be56d0e772485b316ec
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2023-01-17 11:49:03 +0100

    prix total

--- functions.php
+++ functions.php
@@ -1,8 +1,10 @@
 <?php
-//fonction qui calcule le prix total
+/* fonction qui calcule le prix total
+    le paramètre $array correspond à $_SESSION["shopping-cart"] 
+*/
 function prize($array){
     $total_prize=0;
-    foreach($_SESSION["shopping-cart"] as $article){
+    foreach($array as $article){
         $total_prize=$total_prize+$article["price"];
     }
     return $total_prize;
