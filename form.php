c452c85aac3744006cd1984411dae926d61fe539
Nour Everaert <nour.everaert.prof@gmail.com>
2023-01-17 11:33:02 +0100

    avancée form

---
+++ form.php
@@ -0,0 +1,12 @@
+<html>
+<form action="">
+    <label for="fname">First-name : </label>
+    <label for="lname">Last-name : </label>
+    <label for="mail">E-mail : </label>
+    <label for="streetNumber">Street and number : </label>
+    <label for="city">City : </label>
+    <label for="postalCode">Postal code : </label>
+    <label for="country">Country : </label>
+</form>
+
+</html>
