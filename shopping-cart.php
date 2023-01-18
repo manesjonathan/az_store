05e5733cd4ed3ec01eafe436a7dca5774dc9beaf
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2023-01-18 09:16:57 +0100

    ajout des liens

--- shopping-cart.php
+++ shopping-cart.php
@@ -36,7 +36,7 @@ function remove($item)
     <header>
         <h1>AZ Store</h1>
         <nav>
-            <a href="/index.php">Home</a>
+            <a href="./index.php">Home</a>
             <a href="#about">About</a>
             <a href="#product">Product</a>
             <a href="#contact">Contact</a>
