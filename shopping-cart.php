c280a48cb1c4621d8d64ec2313ea6645b24e51be
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2023-01-18 09:35:21 +0100

    modification

--- shopping-cart.php
+++ shopping-cart.php
@@ -63,7 +63,7 @@ function remove($item)
 
     <footer>
         <nav>
-            <a href="/index.php">Home</a>
+            <a href="./index.php">Home</a>
             <a href="#about">About</a>
             <a href="#product">Product</a>
             <a href="#contact">Contact</a>
