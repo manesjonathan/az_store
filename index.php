379fc0b2030b99e78ccd65d59017f0b2ca15a48b
Nour Everaert <nour.everaert.prof@gmail.com>
2023-01-18 13:52:56 +0100

    html page acceuil

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <body>
     <header>
-        <h1>AZ Store</h1>
+        <p>AZ Store</p>
         <nav>
             <a class="active" href="#home">Home</a>
             <a href="#about">About</a>
@@ -22,30 +22,64 @@
         <a href="./shopping-cart.php">Cart</a>
     </header>
     <main>
+        <section>
+            <article>
+                <h1>SHOE THE RIGHT ONE.</h1>
+                <button>See our store</button>
+            </article>
+            <article>
+
+            </article>
+        </section>
         <!-- display the carousel with products-->
 
         <!-- display all products -->
-        <?php
-        session_start();
-        $_SESSION['shopping-cart'] = (isset($_SESSION['shopping-cart'])) ? $_SESSION['shopping-cart'] : array();
-        include "tableau.php";
+        <h2>Our last products</h2>
+        <section>
+            <?php
+            session_start();
+            $_SESSION['shopping-cart'] = (isset($_SESSION['shopping-cart'])) ? $_SESSION['shopping-cart'] : array();
+            include "tableau.php";
 
-        for ($i = 0; $i < count($items); $i++) {
-            $item = $items[$i];
-            echo '<div class="article">';
-            echo '<img src="' . $item['image_url'] . '" class="article__img">';
-            echo '<h3 class="article__name">' . $item['product'] . '</h3>';
-            echo '<p class="article__price">' . $item['price'] . '€</p>';
-            echo '<form method="post" action="" class="article_addcart">';
-            echo '<input type="submit" name="Add' . $i . '" value="Add to cart">';
-            echo '</form>';
-            echo '</div>';
+            for ($i = 0; $i < count($items); $i++) {
+                $item = $items[$i];
+                echo '<div class="article">';
+                echo '<img src="' . $item['image_url'] . '" class="article__img">';
+                echo '<h3 class="article__name">' . $item['product'] . '</h3>';
+                echo '<p class="article__price">' . $item['price'] . '€</p>';
+                echo '<form method="post" action="" class="article_addcart">';
+                echo '<input type="submit" name="Add' . $i . '" value="Add to cart">';
+                echo '</form>';
+                echo '</div>';
 
-            if (isset($_POST["Add{$i}"])) {
-                $_SESSION['shopping-cart'][] = $item;
+                if (isset($_POST["Add{$i}"])) {
+                    $_SESSION['shopping-cart'][] = $item;
+                }
             }
-        }
-        ?>
+            ?>
+        </section>
+        <section>
+            <img src="./assets/image/shoe_two.png" alt="">
+            <h2>WE PROVIDE YOU THE BEST QUALITY.</h2>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+        </section>
+        <section>
+            <article>
+                <img src="./assets/image/image-emily.jpg" alt="">
+                <h4>Emily from xyz</h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+            </article>
+            <article>
+                <img src="./assets/image/image-thomas.jpg" alt="">
+                <h4>Thomas from corporate</h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+            </article>
+            <article>
+                <img src="./assets/image/image-jennie.jpg" alt="">
+                <h4>Jennie from Nike</h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+            </article>
+        </section>
     </main>
 
     <footer>
