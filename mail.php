fa863ad184ccda00430e6de720a172c6ef1ef77e
Jonathan Manes <manesjona@gmail.com>
2023-01-18 12:33:08 +0100

    secure mail

--- mail.php
+++ mail.php
@@ -9,21 +9,23 @@ function send_mail($to)
 {
     $mail = new PHPMailer(true);
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp-relay.sendinblue.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'manesjona@gmail.com';                     // SMTP username
-    $mail->Password   = 'JR7TFvUd6LXDw2jc';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    $mail->Port       = 587;                                    // TCP port to connect to
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp-relay.sendinblue.com';
+    $mail->SMTPAuth   = true;
+    require "./config.php";
+
+    $mail->Username   = $USER_NAME;
+    $mail->Password   = $API_KEY;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
 
     //Recipients
-    $mail->setFrom('manesjona@gmail.com', 'AZ Store');
-    $mail->addAddress($to);     // Add a recipient
+    $mail->setFrom($USER_NAME, 'AZ Store');
+    $mail->addAddress($to);
 
     // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->isHTML(true);
     $mail->Subject = 'Order confirmation';
     $mail->Body    = 'We have received your order, it will be delivery soon !';
 
