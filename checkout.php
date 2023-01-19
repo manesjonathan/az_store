db79733db02ead2a441743d8817c211e76eb21a2
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2023-01-19 14:21:19 +0100

    checkout css

--- checkout.php
+++ checkout.php
@@ -66,6 +66,21 @@ function checkForm($arrayForm)
         }
     }
 }
+
+function display()
+{
+    foreach ($_SESSION['shopping-cart'] as $shopping_item) {
+        //display each element in HTML format
+        ?>
+        <div class="article flex justify-between bg-white text-black my-3">
+            <img src="<?php echo $shopping_item['image_url']; ?>" class="article__img w-10">
+            <h3 class="article__name"><?php echo $shopping_item['product']; ?></h3>
+            <p class="article__quantity"><?php echo $shopping_item['quantity']; ?></p>
+            <p class="article__price"><?php echo $shopping_item['price']; ?>€</p>
+        </div>
+        <?php
+    }
+}
 ?>
 
 
@@ -89,39 +104,48 @@ function checkForm($arrayForm)
         <h2 class="mt-10 text-4xl">Delivery Information</h2>
         <hr class="w-1/2 h-0.5 mx-auto my-4 bg-gray-100 ">
 
-        <section class="mb-auto flex justify-around items-center mt-10">
-            <article class="w-1/2 m-10 p-2 shadow-xl shadow-gray-600">
+        <section class="mb-auto flex flex-wrap justify-around items-center mt-10">
+            <article class=" m-10 p-2 shadow-xl shadow-black w-full sm:w-4/5 lg:w-1/2 xl:w-3/5 ">
                 <h3>Fill in the form below</h3>
-                <form action="" method="post" class="text-white">
-                    <label for="fname">First-name : </label>
-                    <input class="my-1" type="text" id="fname" name="fname" value="<?php echo isset($_POST["fname"]) ? $_POST["fname"] : ''; ?>">
-                    <br>
-                    <label for="lname">Last-name : </label>
-                    <input class="my-1" type="text" id="lname" name="lname" value="<?php echo isset($_POST["lname"]) ? $_POST["lname"] : ''; ?>">
-                    <br>
-                    <label for="mail">E-mail : </label>
-                    <input class="my-1" type="text" id="email" name="email" value="<?php echo isset($_POST["email"]) ? $_POST["email"] : ''; ?>">
-                    <br>
-                    <label for="streetNumber">Street and number : </label>
-                    <input class="my-1" type="text" id="streetNumber" name="streetNumber" value="<?php echo isset($_POST["streetNumber"]) ? $_POST["streetNumber"] : ''; ?>">
-                    <br>
-                    <label for="city">City : </label>
-                    <input class="my-1" type="text" id="city" name="city" value="<?php echo isset($_POST["city"]) ? $_POST["city"] : ''; ?>">
-                    <br>
-                    <label for="postalCode">Postal code : </label>
-                    <input class="my-1" type="text" id="postalCode" name="postalCode" value="<?php echo isset($_POST["postalCode"]) ? $_POST["postalCode"] : ''; ?>">
-                    <br>
-                    <label for="country">Country : </label>
-                    <input class="my-1" type="text" id="country" name="country" value="<?php echo isset($_POST["country"]) ? $_POST["country"] : ''; ?>">
-                    <br>
-                    <input class="bg-blue-600 px-10 py-2 rounded " type="submit"  name="submit" value="Validate">
+                <form action="" method="post" class="text-white flex flex-col center items-center justify-evenly text-left">
+                    <div class="my-1 flex w-3/4 justify-between ">
+                        <label for="fname" class="block">First-name : </label>
+                        <input class="w-1/2" type="text" id="fname" name="fname" value="<?php echo isset($_POST["fname"]) ? $_POST["fname"] : ''; ?>">
+                    </div>
+                    <div class="my-1 flex w-3/4 justify-between">
+                        <label for="lname" class="block">Last-name : </label>
+                        <input class="w-1/2" type="text" id="lname" name="lname" value="<?php echo isset($_POST["lname"]) ? $_POST["lname"] : ''; ?>">
+                    </div>
+                    <div class="my-1 flex w-3/4 justify-between">
+                        <label for="mail" class="block">E-mail : </label>
+                        <input class="w-1/2" type="text" id="email" name="email" value="<?php echo isset($_POST["email"]) ? $_POST["email"] : ''; ?>">
+
+                    </div>
+                    <div class="my-1 flex w-3/4 justify-between">
+                        <label for="streetNumber" class="block">Street and number : </label>
+                        <input class="w-1/2" type="text" id="streetNumber" name="streetNumber" value="<?php echo isset($_POST["streetNumber"]) ? $_POST["streetNumber"] : ''; ?>">
+                    </div>
+                    <div class="my-1 flex w-3/4 justify-between">
+                        <label for="city" class="block">City : </label>
+                        <input class="w-1/2" type="text" id="city" name="city" value="<?php echo isset($_POST["city"]) ? $_POST["city"] : ''; ?>">
+                    </div>
+                    <div class="my-1 flex w-3/4 justify-between">
+                        <label for="postalCode" class="block">Postal code : </label>
+                        <input class="w-1/2" type="text" id="postalCode" name="postalCode" value="<?php echo isset($_POST["postalCode"]) ? $_POST["postalCode"] : ''; ?>">
+                    </div> 
+                    <div class="my-1 flex w-3/4 justify-between">
+                        <label for="country" class="block">Country : </label>
+                        <input class="w-1/2" type="text" id="country" name="country" value="<?php echo isset($_POST["country"]) ? $_POST["country"] : ''; ?>">
+                    </div>
+                    <input class="bg-blue-600 px-10 py-2 rounded cursor-pointer my-1  " type="submit"  name="submit" value="Validate">
                 </form>
             </article>
-            <article class="bg-white w-1/5 text-black">
-                <h3>Shopping cart</h3>
-                <div class="flex justify-between bg-blue-600 px-10 py-2 rounded">
+            <article class="bg-white  text-black w-4/5 sm:w-3/5 lg:w-2/5 xl:w-1/5 ">
+                <h3 class="font-bold">Shopping cart</h3>
+                <?php display(); ?>
+                <div class="flex justify-between bg-blue-600 px-5 py-2 rounded-tl rounded-tr">
                     <p class="block text-white">Total</p>
-                    <p class="block text-white"><?php echo $_SESSION["price"];?></p>
+                    <p class="block text-white"><?php echo $_SESSION["price"];?>€</p>
                 </div>
             </article>
         </section>
